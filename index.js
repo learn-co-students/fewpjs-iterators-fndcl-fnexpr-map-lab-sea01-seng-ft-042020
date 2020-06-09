6394aa839aad2b9b9a72bf4b8908918426ea58a3
johnrusch <johnrusch7@gmail.com>
2020-06-09 16:12:20 -0700

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,16 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+// const titleCased = tutorials.map(function(tutorial) => {
+//   return tutorial.toLowerCase().split(" ").map(function(word){
+//     return (word.charAt(0).toUpperCase() + word.slice(1))
+//   }).join(' ');
+// })
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(tutorial) {
+    return tutorial.split(" ").map(function(word){
+      return (word.charAt(0).toUpperCase() + word.slice(1))
+    }).join(' ');
+  })
 }
